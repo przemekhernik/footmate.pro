4ee329ac1c49c71d4b362eeafe05a419affdb245
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-06-16 21:33:25 +0200

    create module initializers

--- wp-content/themes/footmate/app/App.php
+++ wp-content/themes/footmate/app/App.php
@@ -3,18 +3,48 @@
 namespace FM;
 
 use FM\Core\Core;
+use FM\Integrations\Integrations;
 use FM\Posts\Posts;
 use FM\Teams\Teams;
 
 class App
 {
+    private Core $core;
+
+    private Integrations $integrations;
+
+    private Posts $posts;
+
+    private Teams $teams;
+
     private static ?App $instance = null;
 
     private function __construct()
     {
-        new Core();
-        new Posts();
-        new Teams();
+        $this->core = new Core();
+        $this->integrations = new Integrations();
+        $this->posts = new Posts();
+        $this->teams = new Teams();
+    }
+    
+    public function core(): Core
+    {
+        return $this->core;
+    }
+
+    public function integrations(): Integrations
+    {
+        return $this->integrations;
+    }
+
+    public function posts(): Posts
+    {
+        return $this->posts;
+    }
+
+    public function teams(): Teams
+    {
+        return $this->teams;
     }
 
     private function __clone()
