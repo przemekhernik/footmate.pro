3d14e6354e50402610de7e947fd6405604254ab0
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-06-16 21:10:36 +0200

    add singleton to our app facade

--- wp-content/themes/footmate/app/App.php
+++ wp-content/themes/footmate/app/App.php
@@ -8,10 +8,30 @@ use FM\Teams\Teams;
 
 class App
 {
-    public function __construct()
+    private static ?App $instance = null;
+
+    private function __construct()
     {
         new Core();
         new Posts();
         new Teams();
+    }
+
+    private function __clone()
+    {
+    }
+
+    public function __wakeup()
+    {
+        throw new \Exception('Cannot unserialize a singleton.');
+    }
+
+    public static function get(): App
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new App();
+        }
+
+        return self::$instance;
     }
 }
