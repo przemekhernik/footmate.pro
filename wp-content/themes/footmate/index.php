dada2981480e6743b40b1fdcff7d168d8bf76549
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-07-28 22:11:38 +0200

    remove styles from header

--- wp-content/themes/footmate/index.php
+++ wp-content/themes/footmate/index.php
@@ -5,15 +5,6 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
-
-    <style>
-      body { max-width: 1024px; margin: auto; }
-      p { margin: 0 0 1.375rem 0; }
-      ul { padding: 0 0 0 20px }
-      .app { display: flex; }
-      .app__content { flex-basis: 75%; max-width: 75%; padding: 20px; }
-      .app__sidebar { flex-basis: 25%; max-width: 25%; padding: 20px; }
-    </style>
   </head>
 
   <body <?php body_class(); ?>>
