afdf7be5f36d2cdb49212cf320b7e2df7217a002
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-07-28 22:10:27 +0200

    add small fixes

--- wp-content/themes/footmate/app/App.php
+++ wp-content/themes/footmate/app/App.php
@@ -2,45 +2,63 @@
 
 namespace FM;
 
-use FM\Core\Core;
 use FM\Core\Config;
+use FM\Core\Hooks;
+use FM\Core\Setup;
+use FM\Core\Widgets;
 use FM\Integrations\Integrations;
 use FM\Posts\Posts;
 use FM\Teams\Teams;
+use Illuminate\Filesystem\Filesystem;
 
 class App
 {
-    private Core $core;
+    private Config $config;
+
+    private Filesystem $filesystem;
 
     private Integrations $integrations;
 
     private Posts $posts;
 
+    private Setup $setup;
+
     private Teams $teams;
+
+
+    private Widgets $widgets;
 
     private static ?App $instance = null;
 
     private function __construct()
     {
-        $this->core = self::init(new Core());
+        $this->config = self::init(new Config());
+        $this->filesystem = new Filesystem();
         $this->integrations = self::init(new Integrations());
         $this->posts = self::init(new Posts());
+        $this->setup = self::init(new Setup());
         $this->teams = self::init(new Teams());
-    }
-    
-    public function core(): Core
-    {
-        return $this->core;
+        $this->widgets = self::init(new Widgets());
     }
 
     public function config(): Config
     {
-        return $this->core->config();
+        return $this->config;
+    }
+
+    public function filesystem(): Filesystem
+    {
+        return $this->filesystem;
     }
 
     public function integrations(): Integrations
     {
         return $this->integrations;
+    }
+
+    public function setup(): Setup
+    {
+        return $this->setup;
     }
 
     public function posts(): Posts
@@ -51,6 +69,11 @@ class App
     public function teams(): Teams
     {
         return $this->teams;
+    }
+
+    public function widgets(): Widgets
+    {
+        return $this->widgets;
     }
 
     private function __clone()
@@ -73,6 +96,6 @@ class App
 
     public static function init(object $module): object
     {
-        return \FM\Core\Hooks::init($module);
+        return Hooks::init($module);
     }
 }
