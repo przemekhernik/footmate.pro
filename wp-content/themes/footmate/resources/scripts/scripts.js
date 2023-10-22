4b14e71ed743de02e70c472f735154c7c89fafdb
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-10-22 22:45:50 +0200

    hot module reload

--- wp-content/themes/footmate/resources/scripts/scripts.js
+++ wp-content/themes/footmate/resources/scripts/scripts.js
@@ -1,1 +1,3 @@
+import './test.js';
+
 console.log('main');
---
+++ wp-content/themes/footmate/resources/scripts/test.js
@@ -0,0 +1,33 @@
+let count = 0;
+let interval = null;
+
+function create() {
+  interval = setInterval(() => {
+    count++;
+    document.getElementById('count').innerText = count;
+  }, 1000)
+
+  document.getElementById('message').innerText = 'Change message'
+}
+
+function destroy () {
+  clearInterval(interval)
+}
+
+export function setState(val) {
+  count = val;
+}
+
+
+
+create()
+
+if (import.meta.hot) {
+    import.meta.hot.accept((module) => {
+      module.setState(import.meta.hot.data.count)
+    })
+    import.meta.hot.dispose(() => {
+      import.meta.hot.data.count = count;
+      destroy();
+    })
+  }
