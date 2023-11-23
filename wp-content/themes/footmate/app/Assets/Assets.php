84b3af82005bfed9bedf5f3a2cb9ed9581f3ab0b
Przemysław Hernik <przemek.hernik@hotmail.com>
2023-11-23 22:17:35 +0100

    small change

--- wp-content/themes/footmate/app/Assets/Assets.php
+++ wp-content/themes/footmate/app/Assets/Assets.php
@@ -17,14 +17,4 @@ class Assets
         wp_enqueue_script('theme', $this->resolve('scripts/scripts.js'), [], fm()->config()->get('version'));
         wp_enqueue_script('theme-blocks', $this->resolve('scripts/blocks.js'), [], fm()->config()->get('version'));
     }
-
-    /**
-     * @filter script_loader_tag 1 3
-     */
-    public function module(string $tag, string $handle, string $url): string
-    {
-        $tag = str_replace('<script ', '<script type="module" ', $tag);
-
-        return $tag;
-    }
 }
