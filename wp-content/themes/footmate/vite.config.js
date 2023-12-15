2f8fc948b8f8ad2a0d012f447970aa8707c5f734
Przemysław Hernik <przemek.hernik@coditive.com>
2023-12-15 16:16:54 +0100

    working commit

--- wp-content/themes/footmate/vite.config.js
+++ wp-content/themes/footmate/vite.config.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 import autoprefixer from 'autoprefixer';
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import { babel } from '@rollup/plugin-babel';
 
@@ -13,7 +16,6 @@ export default defineConfig({
     assetsDir: '.',
     outDir: `dist`,
     emptyOutDir: true,
-    sourcemap: true,
     rollupOptions: {
       input: [
         'resources/scripts/scripts.js',
@@ -21,10 +23,63 @@ export default defineConfig({
         'resources/scripts/blocks.js',
       ],
       output: {
-        entryFileNames: '[hash].js',
-        assetFileNames: '[hash].[ext]',
+        entryFileNames: 'scripts/[hash].js',
+        chunkFileNames: 'vendor/[hash].js',
+        assetFileNames: function(info) {
+          switch (path.extname(info.name)) {
+            case '.woff':
+            case '.woff2':
+              return 'fonts/[hash].[ext]';
+
+            case '.js':
+              return 'scripts/[hash].[ext]';
+
+            case '.css':
+            case '.scss':
+              return 'styles/[hash].[ext]';
+
+            case '.gif':
+            case '.png':
+            case '.jpg':
+            case '.jpeg':
+            case '.svg':
+            case '.webp':
+              return 'images/[name].[ext]';
+          
+            default:
+              return 'others/[hash].[ext]';
+          }
+        },
       },
       plugins: [
+        copy({
+          targets: [
+            { src: `resources/images/**/*`, dest: `dist/images`, rename: function(name, ext, fullPath) {
+              const nname = `${name}-[hash].${ext}`;
+              const nnname = crypto.randomBytes(4).toString('hex') + `.${ext}`;
+
+              try {
+                const mpath = `${ROOT}${BASE}/dist/manifest.json`;
+                const manifestContent = fs.readFileSync(mpath, 'utf-8');
+                const manifest = JSON.parse(manifestContent);
+
+                manifest[fullPath] = {
+                  file: nnname,
+                  isEntry: false,
+                  src: fullPath,
+                };
+
+                fs.writeFileSync(mpath, JSON.stringify(manifest, null, 2));
+              } catch (error) {
+                console.error('Error updating manifest file:', error);
+              }
+              
+
+              return nnname;
+            } },
+          ],
+          hook: 'writeBundle'
+        }),
         babel({
           babelHelpers: 'bundled',
           presets: [
