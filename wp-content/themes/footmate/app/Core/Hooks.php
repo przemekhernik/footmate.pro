5958ecc0263a32073c731aa5cf5f6086da8e2bf6
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-01-26 00:20:07 +0100

    make problem

--- wp-content/themes/footmate/app/Core/Hooks.php
+++ wp-content/themes/footmate/app/Core/Hooks.php
@@ -5,8 +5,7 @@ class Hooks
 {
     private const PATTERN = '#\* @(?P<type>filter|action|shortcode)\s+(?P<name>[a-z0-9\-\.\/_]+)(\s+(?P<priority>\d+))?#';
 
-    public static function init(object $object): object
-    {   
+    public static function init(object $object): object {   
         foreach (self::extract($object) as $config) {
             call_user_func($config['hook'], $config['name'], $config['callback'], $config['priority'], $config['args']);
         }
