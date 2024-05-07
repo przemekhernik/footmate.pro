c41cd879dcb12bb18349ac9a97321c034b5b3038
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-05-07 21:29:01 +0200

    add id to block

--- wp-content/themes/footmate/app/Blocks/Base.php
+++ wp-content/themes/footmate/app/Blocks/Base.php
@@ -6,8 +6,25 @@ use FM\Blocks\Block;
 
 class Base extends Block
 {
+    private string $id = '';
+
+    public function __construct()
+    {
+        $this->setId('base');
+    }
+
     protected function getTemplate(): string
     {
-        return sprintf('%s/base/template.blade.php', fm()->config()->get('blocks.path'));
+        return sprintf('%s/%s/template.blade.php', fm()->config()->get('blocks.path'), $this->getId());
+    }
+
+    protected function getId(): string
+    {
+        return $this->id;
+    }
+
+    protected function setId(string $id): void
+    {
+        $this->id = $id;
     }
 }
