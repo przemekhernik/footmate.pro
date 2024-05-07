be60687188970488fe17228e1d781c5bb5f1f49d
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-05-07 21:39:15 +0200

    move id setup to abstract class

--- wp-content/themes/footmate/app/Blocks/Base.php
+++ wp-content/themes/footmate/app/Blocks/Base.php
@@ -6,25 +6,8 @@ use FM\Blocks\Block;
 
 class Base extends Block
 {
-    private string $id = '';
-
     public function __construct()
     {
         $this->setId('base');
-    }
-
-    protected function getTemplate(): string
-    {
-        return sprintf('%s/%s/template.blade.php', fm()->config()->get('blocks.path'), $this->getId());
-    }
-
-    protected function getId(): string
-    {
-        return $this->id;
-    }
-
-    protected function setId(string $id): void
-    {
-        $this->id = $id;
     }
 }
--- wp-content/themes/footmate/app/Blocks/Block.php
+++ wp-content/themes/footmate/app/Blocks/Block.php
@@ -4,6 +4,8 @@ namespace FM\Blocks;
 
 abstract class Block
 {
+    private string $id = '';
+
     public function render(array $data = []): void
     {
         fm()->templates()->render($this->getTemplate(), $data);
@@ -14,5 +16,22 @@ abstract class Block
         return fm()->templates()->generate($this->getTemplate(), $data);
     }
 
-    abstract protected function getTemplate(): string;
+    protected function getId(): string
+    {
+        if (empty($this->id)) {
+            throw new \Exception('Block ID is missing.');
+        }
+
+        return $this->id;
+    }
+
+    protected function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    protected function getTemplate(): string
+    {
+        return sprintf('%s/%s/template.blade.php', fm()->config()->get('blocks.path'), $this->getId());
+    }
 }
