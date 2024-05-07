6f7a0832d3a63d985a05668bf83731768c28258f
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-05-07 21:41:11 +0200

    add final keywords to abstract class

--- wp-content/themes/footmate/app/Blocks/Block.php
+++ wp-content/themes/footmate/app/Blocks/Block.php
@@ -16,7 +16,7 @@ abstract class Block
         return fm()->templates()->generate($this->getTemplate(), $data);
     }
 
-    protected function getId(): string
+    final protected function getId(): string
     {
         if (empty($this->id)) {
             throw new \Exception('Block ID is missing.');
@@ -25,12 +25,12 @@ abstract class Block
         return $this->id;
     }
 
-    protected function setId(string $id): void
+    final protected function setId(string $id): void
     {
         $this->id = $id;
     }
 
-    protected function getTemplate(): string
+    final protected function getTemplate(): string
     {
         return sprintf('%s/%s/template.blade.php', fm()->config()->get('blocks.path'), $this->getId());
     }
