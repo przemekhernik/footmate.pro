d1cb043affc81b82000135305fc116cdab5f67ec
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-05-08 21:36:05 +0200

    move the native resources to other directory

--- wp-content/themes/footmate/resources/functions.php
+++ wp-content/themes/footmate/resources/functions.php
@@ -1,8 +1,8 @@
 <?php // phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
 
 define('FM_VERSION', '0.1.1');
-define('FM_ROOT', str_replace(ABSPATH, '/', dirname(__FILE__)));
-define('FM_PATH', dirname(__FILE__));
+define('FM_ROOT', str_replace(ABSPATH, '/', dirname(__FILE__, 2)));
+define('FM_PATH', dirname(__FILE__, 2));
 define('FM_URI', home_url(FM_ROOT));
 define('FM_HMR_HOST', 'http://localhost:5173');
 define('FM_HMR_URI', FM_HMR_HOST . FM_ROOT);
