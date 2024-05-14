c1d50cc5f106344e01563f146575ae511e4045ec
Przemysław Hernik <przemek.hernik@hotmail.com>
2024-05-14 21:52:19 +0200

    add assets

---
+++ wp-content/themes/footmate/resources/blocks/base/script.js
@@ -0,0 +1,9 @@
+class Base {
+  constructor(element) {
+    this.element = element;
+  }
+}
+
+document.querySelectorAll('[data-block="base"]').forEach(block => {
+  new Base(block); // eslint-disable-line no-new
+});
