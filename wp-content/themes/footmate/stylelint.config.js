d14b6d2b492adfcfbce9bb6e806c9621232e1f24
Przemysław Hernik <przemek.hernik@hotmail.com>
2025-01-28 22:01:30 +0100

    resign from prettier in styles - remove css from prettier - update stylelint - add stylistic

--- wp-content/themes/footmate/stylelint.config.js
+++ wp-content/themes/footmate/stylelint.config.js
@@ -1,12 +1,17 @@
 import { propertyGroups } from 'stylelint-config-clean-order';
 
 export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-clean-order',
+    '@stylistic/stylelint-config',
+  ],
   rules: {
+    'at-rule-no-unknown': null,
     'no-empty-source': null,
     'selector-class-pattern': null,
     'order/properties-order': [
-      propertyGroups.map((properties) => ({
+      propertyGroups.map(properties => ({
         noEmptyLineBetween: true,
         emptyLineBefore: 'never',
         properties,
@@ -16,5 +21,6 @@ export default {
         unspecified: 'bottomAlphabetical',
       },
     ],
+    '@stylistic/string-quotes': 'single',
   },
 };
